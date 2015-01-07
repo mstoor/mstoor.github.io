f1ca2a49d7dd9a364baf5704b6286e84624f63ff
h01 <root@ursb.org>
2015-01-07 11:32:59 +0800

    更新服务器

--- static/mst/js/config.js
+++ static/mst/js/config.js
@@ -1,1 +1,5 @@
-var mst_config_server = "http://127.0.0.1:2048";
+//	@name	MST2.0
+//	@desc	代码写得很烂，兄台不要再看
+//	@create	2014/12/01
+//	@modify	2015/01/06
+var mst_config_server = "http://mst-server.3asec.com:2048";
